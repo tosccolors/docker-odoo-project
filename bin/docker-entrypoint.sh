a1dd718f888c73fc43871f895579ed65cdbfaade
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-05-20 09:33:56 +0200

    Last fixes

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -7,6 +7,13 @@ export PGUSER=${DB_USER}
 export PGPASSWORD=${DB_PASSWORD}
 export PGDATABASE=${DB_NAME}
 
+export USER="${USER_NAME:-odoo}"
+if ! whoami &> /dev/null; then
+    if [ -w /etc/passwd ]; then
+        echo "${USER}:x:$(id -u):0:${USER} user:${HOME}:/sbin/nologin" >> /etc/passwd
+    fi
+fi
+
 # Accepted values for DEMO: True / False
 # Odoo use a reverse boolean for the demo, which is not handy,
 # that's why we propose DEMO which exports WITHOUT_DEMO used in
@@ -82,12 +89,17 @@ fi
 wait_postgres.sh
 
 mkdir -p /data/odoo/{addons,filestore,sessions}
-if [ ! "$(stat -c '%U' /data/odoo)" = "${USER}" ]; then
-  chown -R ${USER}: /data/odoo
+# FIXME still required?
+if [ ! "$(stat -c '%U' /data/odoo/filestore)" = "${USER}" ]; then
+  chown -R ${USER}: /data/odoo/filestore
 fi
-if [ ! "$(stat -c '%U' /var/log/odoo)" = "${USER}" ]; then
-  chown -R ${USER}: /var/log/odoo
+  if [ ! "$(stat -c '%U' /data/odoo/sessions)" = "${USER}" ]; then
+  chown -R ${USER}: /data/odoo/sessions
 fi
+# TODO drop /var/log/odoo?
+# if [ ! "$(stat -c '%U' /var/log/odoo)" = "${USER}" ]; then
+#  chown -R ${USER}: /var/log/odoo
+# fi
 
 BASE_CMD=$(basename $1)
 if [ "$BASE_CMD" = "odoo" ] || [ "$BASE_CMD" = "odoo.py" ] ; then
@@ -98,7 +110,7 @@ if [ "$BASE_CMD" = "odoo" ] || [ "$BASE_CMD" = "odoo.py" ] ; then
   fi
 
   if [ -z "$MIGRATE" -o "$MIGRATE" = True ]; then
-    gosu odoo migrate
+    migrate
   fi
 
   START_ENTRYPOINT_DIR=/start-entrypoint.d
