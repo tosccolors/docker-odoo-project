6475628642307cda81b365f15339c8f6eea70ad5
Willem Hulshof <w.hulshof@magnus.nl>
2020-02-11 01:18:38 +0100

    spaces

--- install/dev_package.sh
+++ install/dev_package.sh
@@ -7,14 +7,14 @@ equivs-control python-pypdf
 rm python-pypdf
 touch python-pypdf
 echo "Section: python
- Package: python-pypdf
- Version: 1.13
- Architecture: all
- Description: fake package to provide python-pypdf
- This package provide the dependency needed by Odoo.
- python-pypdf2 replace python-pypdf, but while compatible,
- does not provide python-pypdf" >> python-pypdf
- 
+Package: python-pypdf
+Version: 1.13
+Architecture: all
+Description: fake package to provide python-pypdf
+This package provide the dependency needed by Odoo.
+python-pypdf2 replace python-pypdf, but while compatible,
+does not provide python-pypdf" >> python-pypdf
+
 cat python-pypdf
 equivs-build python-pypdf
 dpkg -i python-pypdf_1.13_all.deb
