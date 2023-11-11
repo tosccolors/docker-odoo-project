a4687959634ddcf3c00ad5bfd30931ef837f2323
Willem Hulshof <whulshof@tosc.nl>
2023-11-11 13:50:54 +0100

    UID=999 --> UID=1001

--- test.sh
+++ test.sh
@@ -42,18 +42,18 @@ trap on_exit EXIT
 # run 'runtests' in the container
 # extra arguments are passed to the 'run' command (example: -e FOO=bar is added to the list of args)
 docoruntests() {
-    docker-compose -f test-compose.yml run --rm -e LOCAL_USER_ID=999 $@ odoo runtests
+    docker-compose -f test-compose.yml run --rm -e LOCAL_USER_ID=1001 $@ odoo runtests
 }
 # run 'runmigration' in the container
 # extra arguments are passed to the 'run' command (example: -e FOO=bar is added to the list of args)
 docorunmigration() {
-    docker-compose -f test-compose.yml run --rm -e LOCAL_USER_ID=999 $@ odoo runmigration
+    docker-compose -f test-compose.yml run --rm -e LOCAL_USER_ID=1001 $@ odoo runmigration
 }
 docodown() {
     docker-compose -f test-compose.yml down
 }
 docoruncmd() {
-    docker-compose -f test-compose.yml run --rm -e LOCAL_USER_ID=999 $@
+    docker-compose -f test-compose.yml run --rm -e LOCAL_USER_ID=1001 $@
 }
 
 cp -ra ./example/. "$TMP/"
